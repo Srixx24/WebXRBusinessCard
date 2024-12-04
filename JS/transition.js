7e4719d4bd1e2dc65a5f525cf947cfc311c1449e
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-12-04 08:32:16 -0600

    Edits

--- JS/transition.js
+++ JS/transition.js
@@ -4,20 +4,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const overlay = document.getElementById('overlay');
     const welcomeScreen = document.getElementById('welcome-screen');
     const logo = document.getElementById('logo');
-    if (overlay) {
+    const scene = document.getElementById('scene');
+
         overlay.addEventListener('click', function() {
-            if (welcomeScreen && logo) {
-                // Fade out
-                welcomeScreen.style.opacity = '0';
-                logo.style.opacity = '0';
-                
-                // Wait for the transition to complete before hiding elements
-                setTimeout(() => {
-                    welcomeScreen.style.display = 'none';
-                    logo.style.display = 'none';
-                    overlay.style.display = 'none';
-                }, 1000);
-            }
+            // Fade out welcome screen and logo
+            welcomeScreen.style.opacity = '0';
+            logo.style.opacity = '0';
+
+            setTimeout(() => {
+                welcomeScreen.style.display = 'none';
+                logo.style.display = 'none';
+                overlay.style.display = 'none';
+
+                // Show the A-Frame scene
+                scene.style.display = 'block';
+            }, 1000);
         });
-    }
 });
