2f4982d71dc07d021578dc6168431e77024fdc9c
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-12-11 16:26:44 -0600

    Edits

--- JS/arIcons.js
+++ JS/arIcons.js
@@ -1,5 +1,18 @@
 // Handles events for AR scene only
 
 document.addEventListener('DOMContentLoaded', () => {
-    
-})
+    const githubIcon = document.getElementById('Github');
+    const linkedinIcon = document.getElementById('Linkedin');
+    const gmailIcon = document.getElementById('GmailIcon');
+    const toggleIcon = document.getElementById('Toggle');
+
+    githubIcon.addEventListener('click', function() {
+        window.open('https://github.com/Srixx24/', '_blank');
+    });
+    linkedinIcon.addEventListener('click', function() {
+        window.open('https://www.linkedin.com/in/jackielovins/', '_blank');
+    });
+    gmailIcon.addEventListener('click', function() {
+        window.open('mailto:jacolynlovins@gmail.com');
+    });
+});
