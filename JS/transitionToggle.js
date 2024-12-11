a59fdd79086da6558cebfd62f95f676c9d7528c5
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-12-11 13:08:41 -0600

    Edits

--- JS/transitionToggle.js
+++ JS/transitionToggle.js
@@ -26,17 +26,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // Toggle between AR and forest scene
         let isARMode = true;
-        document.getElementById('Scene-toggle').addEventListener('click', () => {
-            isARMode = !isARMode;
+        
+        // AR scene toggle
+        document.getElementById('ar-toggle').addEventListener('click', () => {
 
             if (isARMode) {
-                forestScene.style.display = 'none';
-                arScene.style.display = 'block'; // Show AR camera scene
-                document.getElementById('Toggle-label').setAttribute('value', 'Press black rock to go to Forest');
-            } else {
                 arScene.style.display = 'none';
-                forestScene.style.display = 'block'; // Show forest scene
-                document.getElementById('Toggle-label').setAttribute('value', 'Press black rock to go to AR View');
+                forestScene.style.display = 'block';
+                document.getElementById('forest-label').setAttribute('value', 'Press black rock to go to AR View');
+                isARMode = false; // Update to Forest
+            }
+        });
+        // Forest scene toggle
+        document.getElementById('forest-toggle').addEventListener('click', () => {
+
+            if (!isARMode) {
+                forestScene.style.display = 'none';
+                arScene.style.display = 'block';
+                document.getElementById('ar-label').setAttribute('value', 'Press the kitten to go to Forest');
+                isARMode = true; // Update to AR
             }
         });
     }
