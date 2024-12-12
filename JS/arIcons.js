c328406b3d868b1103d022f3a049837c4bba2d71
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-12-11 22:49:00 -0600

    Edits

--- JS/arIcons.js
+++ JS/arIcons.js
@@ -5,17 +5,53 @@ document.addEventListener('DOMContentLoaded', () => {
     const linkedinIcon = document.getElementById('Linkedin');
     const gmailIcon = document.getElementById('Gmail');
     const toggleIcon = document.getElementById('Toggle');
+    const customCursor = document.getElementById('custom-cursor');
 
+    function changeCursorColor(color) {
+        customCursor.setAttribute('color', color); // Change cursor color on hover
+    }
+
+    // Github events
     githubIcon.addEventListener('click', function() {
         window.open('https://github.com/Srixx24/', '_blank');
     });
+    githubIcon.addEventListener('mouseenter', () => {
+        changeCursorColor('#099631');
+    });
+    githubIcon.addEventListener('mouseleave', () => {
+        changeCursorColor('#FF0000');
+    });
+
+    // Linkedin events
     linkedinIcon.addEventListener('click', function() {
         window.open('https://www.linkedin.com/in/jackielovins/', '_blank');
     });
+    linkedinIcon.addEventListener('mouseenter', () => {
+        changeCursorColor('#099631');
+    });
+    linkedinIcon.addEventListener('mouseleave', () => {
+        changeCursorColor('#FF0000');
+    });
+
+    // Gmail events
     gmailIcon.addEventListener('click', function() {
         window.open('mailto:jacolynlovins@gmail.com');
     });
+    gmailIcon.addEventListener('mouseenter', () => {
+        changeCursorColor('#099631');
+    });
+    gmailIcon.addEventListener('mouseleave', () => {
+        changeCursorColor('#FF0000');
+    });
+
+    // Forest Scene events
     toggleIcon.addEventListener('click', function() {
         window.location.href = 'Scenes/forest.html';
+    });
+    toggleIcon.addEventListener('mouseenter', () => {
+        changeCursorColor('#099631');
+    });
+    toggleIcon.addEventListener('mouseleave', () => {
+        changeCursorColor('#FF0000');
     });
 });
